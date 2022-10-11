81015d6d490d6b4aa4e1220eae0ed3119f83db62
SamnangNhum <68495204+SamnangNhum@users.noreply.github.com>
2022-10-11 16:54:47 +0700

    Nest 9

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors , ParseFi } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { productDto } from 'src/dto/product.dto';
 import { ProductService } from './product.service';
+import { diskStorage } from 'multer';
+import { extname } from 'path'
 
 @Controller('product')
 export class ProductController {
@@ -17,4 +20,24 @@ export class ProductController {
         
     }
 
-}
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file' ,  {
+        storage: diskStorage({
+            destination: './uploads'
+            , filename: (req, file, cb) => {
+              // Generating a 32 random chars long string
+              const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+              //Calling the callback passing the random name generated with the original extension name
+              cb(null, `${randomName}${extname(file.originalname)}`)
+            }
+          })
+        
+    }) )
+    uploadThumbnail(@UploadedFile(new ParseFilePipe({
+    
+      }),) file: Express.Multer.File) {
+        return 'upload successfully uploaded';
+       }
+    }
+
+
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
 import { Product } from './product.entity';
 import { ProductService } from './product.service';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Product])],
-  controllers:[ProductController],
-  providers: [ProductService]
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    MulterModule.register({
+      dest: './upload',            
+      
+    }),
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
 })
 export class ProductModule {}
